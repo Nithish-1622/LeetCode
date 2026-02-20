3eef5657474751e5c28e3d523dc2b78def33ef8c
Nithish S <nithish.s.1622@gmail.com>
2026-02-20 11:56:47 +0530

    Time : 12 ms (12.2%)  Space : 20.8 MB (24.39%)  Date : 02-20-2026

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+       int n = nums.size() , dis = 0;
+       for ( int i=0 ; i<n ; i++){
+            dis = dis ^ nums[i];
+       } 
+       return dis;
+    }
+};
