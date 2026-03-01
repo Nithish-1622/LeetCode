3ba089875aa1a1d773d5d371302361e7ca2e42d4
Nithish S <nithish.s.1622@gmail.com>
2026-03-01 18:09:03 +0530

    Time : 3 ms (90%)  Space : 31.1 MB (30%)  Date : 03-01-2026

---
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> minDistinctFreqPair(vector<int>& nums) {
+        int c = 0;
+        map<int,int> freq;
+        for(int i:nums){
+            freq[i]++;
+        }
+        vector<int> uniq;
+        for(auto &p:freq){
+            uniq.push_back(p.first);
+        }
+        
+        for(int i=0;i<uniq.size();i++){
+            for(int j=i+1;j<uniq.size();j++){
+                int x = uniq[i];
+                int y = uniq[j];
+                if(freq[x]!=freq[y]){
+                    return {x,y};
+                }
+            }
+        }
+
+    return {-1,-1};
+    }
+};
