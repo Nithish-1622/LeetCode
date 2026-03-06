f372dd3f784982015593a8fa7bf4dbe2fb5b0570
Nithish S <nithish.s.1622@gmail.com>
2026-03-06 21:36:55 +0530

    Time : 6 ms (56.06%)  Space : 10.1 MB (29.26%)  Date : 03-06-2026

---
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while(s.size()>2){
+        string str="";
+        for(int i=0;i<s.size()-1;i++){
+            str+=(char)((s[i]-'0')+(s[i+1]-'0'))%10;
+
+        }
+        s=str;
+        }
+        if(s[0]==s[1]) return true;
+        return false;
+    }
+};
