625f1e69cecd2e85cbdda6f95181b5685eb6f6b9
Nithish S <nithish.s.1622@gmail.com>
2026-03-09 21:15:05 +0530

    Time : N/A (0%)  Space : N/A (0%)  Date : 03-09-2026

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     string removeTrailingZeros(string num) {
         int n = num.length();
-        for(int i=n-1;i>=0;i--){
-            if(num[i]=='0'){
-                num.pop_back();
-            }else{
-                break;
-            }
+        while(num.back() == '0'){
+            num.pop_back();
         }
         return num;
     }
