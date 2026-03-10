5255071a5b809ec2bc5a30f9341ab1ae331fd2ca
Nithish S <nithish.s.1622@gmail.com>
2026-03-10 10:23:31 +0530

    Time : 0 ms (100%)  Space : 10 MB (63.41%)  Date : 03-10-2026

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode* temp = head;
+        int c = 0;
+        while(temp!=nullptr){
+            c++;
+            temp = temp->next;
+        } 
+        int mid = (c/2);
+        temp = head;
+        for(int i=0;i<mid;i++){
+            temp=temp->next;
+        }
+        return temp;
+
+    }
+};
