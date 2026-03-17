a1d5cbf21622f5adb3b8e2efa4c4c17e8962b28c
Nithish S <nithish.s.1622@gmail.com>
2026-03-17 22:27:02 +0530

    Time : N/A (0%)  Space : N/A (0%)  Date : 03-17-2026

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -12,12 +12,18 @@ class Solution {
 public:
     ListNode* sortList(ListNode* head) {
         vector<int> arr;
+
+        if(head == NULL || head->next == NULL) return head;
+
         ListNode* temp = head;
+
         while(temp != nullptr){
             arr.push_back(temp->val);
             temp = temp->next;
-        }     
+        }
+
         sort(arr.begin(),arr.end());
+
         temp = head;
         for(int i=0 ; i<arr.size() ; i++){
             temp->val = arr[i];
@@ -25,7 +31,5 @@ public:
         }
 
         return head;
-
-
     }
 };
