9390db3389b1b2efea5c705c2514f9647c1ac497
Nithish S <nithish.s.1622@gmail.com>
2026-04-27 07:47:33 +0530

    Time : 0 ms (100%)  Space : 45.4 MB (1.6%)  Date : 04-27-2026

--- 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int c = 0;
-        for(int i=0;i<nums.size();i++){
-            c+=nums[i];
-        }
-        return c%k;
+        return accumulate(nums.begin(),nums.end(),0)%k;
     }
 };
