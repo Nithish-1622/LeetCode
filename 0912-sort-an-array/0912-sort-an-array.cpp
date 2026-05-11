9d8f2704a658f90ac9c604c8007be875917da375
Nithish S <nithish.s.1622@gmail.com>
2026-05-11 23:46:05 +0530

    Time : 19 ms (89.79%)  Space : 70.8 MB (99.23%)  Date : 05-11-2026

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if (nums[i]>nums[j]){
+        //             swap (nums[i],nums[j]);
+        //         }
+        //     }
+        // }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
