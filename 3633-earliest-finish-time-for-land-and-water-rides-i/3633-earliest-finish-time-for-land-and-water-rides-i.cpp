97e2646d02511ca99592f015e34902bc824c2186
Nithish S <nithish.s.1622@gmail.com>
2026-06-02 18:07:03 +0530

    Time : 10 ms (40.93%)  Space : 91.9 MB (59.92%)  Date : 06-02-2026

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -9,6 +9,7 @@ public:
                 int land = landStartTime[i] + landDuration[i];
                 int land_water = max(land, waterStartTime[j]) + waterDuration[j];
                 res = min(res, land_water);
+                
                 int water = waterStartTime[j] + waterDuration[j];
                 int water_land = max(water, landStartTime[i]) + landDuration[i];
                 res = min(res, water_land);
