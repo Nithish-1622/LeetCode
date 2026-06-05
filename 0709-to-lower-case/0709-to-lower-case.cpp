533ae48be4224ca39cef8b6ba07a8121cd712002
Nithish S <nithish.s.1622@gmail.com>
2026-06-05 12:31:01 +0530

    Time : 0 ms (100%)  Space : 8.3 MB (53.75%)  Date : 06-05-2026

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(char &x: s){
-            x = tolower(x);
+        string ans;
+        for(int i=0;i<s.size();i++)
+        {
+            char c = s[i];
+             if (c >= 'A' && c <= 'Z') {
+                c = c - 'A' + 'a';
+            }
+            ans += c; 
         }
-        return s;
+        return ans;
     }
 };
