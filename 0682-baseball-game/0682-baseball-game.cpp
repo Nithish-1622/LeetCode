5c114ed5e32bb87822068b41e51bcf6598ccc53a
Nithish S <nithish.s.1622@gmail.com>
2026-06-25 13:35:05 +0530

    Time : 4 ms (4.72%)  Space : 13 MB (16.78%)  Date : 06-25-2026

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int calPoints(vector<string>& o) {
+        int n = o.size();
+        stack<int> st;
+        int sc = 0;
+        for(string x : o){
+            if (x=="D"){
+                st.push(2*st.top());
+            }else if (x=="C"){
+                st.pop();
+            } else if(x=="+"){
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.push(a);
+                st.push(a+b); 
+            } else{
+                st.push(stoi(x));
+            }
+        }
+        if (st.empty())
+            return 0;
+        while (!st.empty()) {
+            sc += st.top();
+            st.pop();
+        }
+        return sc;
+    }
+};
