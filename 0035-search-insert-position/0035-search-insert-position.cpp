01e5878255a403244de7662ecfe59044d959695b
Nithish S <nithish.s.1622@gmail.com>
2026-07-12 19:22:38 +0530

    Time : 0 ms (100%)  Space : 13.6 MB (78.03%)  Date : 07-12-2026

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,20 +3,8 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int l = 0;
         int r = nums.size()-1;
-        while(l<=r){
-            int mid_element = l + (r-l) / 2;
-
-            if(nums[mid_element]==target){
-                return mid_element;
-            }
-            else if(nums[mid_element]<target){
-                l = mid_element+1;
-            }
-            else{
-                r = mid_element-1;
-            }
-        }
-        return l;
+        int ans = lower_bound(nums.begin(),nums.end(),target) - nums.begin();
+        return ans;
 
     }
 };
