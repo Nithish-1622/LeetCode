718cc9a159fa9c05a3dea35daccb716dbcdd7f61
Nithish S <nithish.s.1622@gmail.com>
2026-07-22 15:25:17 +0530

    Time : 87 ms (13.43%)  Space : 113.4 MB (6.69%)  Date : 07-22-2026

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for (int x: nums){
+            mp[x]++;
+        }
+        for(auto p: mp){
+            if(p.second != 1) return true;
+        }
+        return false;
+        
+    }
+};
