c3a8c5bef99f87f82c431e030f5cb67ec5705bf2
Nithish S <nithish.s.1622@gmail.com>
2026-07-28 19:53:54 +0530

    Time : 0 ms (100%)  Space : 11.3 MB (90.23%)  Date : 07-28-2026

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLen = 0;
+        int hash[256];
+        for (int i = 0; i < 256; ++i) {
+            hash[i] = -1;
+        }
+        int l = 0, r = 0;
+        int n = s.size();
+        int len = 0;
+        while(r<n){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l = hash[s[r]]+1;
+                }
+            }
+            len  = r-l+1;
+            maxLen = max(maxLen,len);
+            hash[s[r]] = r;
+            r++;
+        }
+        return maxLen;
+        
+    }
+};
