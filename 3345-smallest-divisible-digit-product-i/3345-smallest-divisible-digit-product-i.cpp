b9ba349e84ff88d148988f63f59b99eee2a87026
Nithish S <nithish.s.1622@gmail.com>
2026-08-08 09:08:45 +0530

    Time : 0 ms (100%)  Space : 8.6 MB (75.19%)  Date : 08-08-2026

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
+    int div_mul(int n){
+        int m = 1;
+        while(n){
+            int a = n%10;
+            m = m * a;
+            n/=10;
+        }
+        return m;
+    }
     int smallestNumber(int n, int t) {
-        auto [q, r] = div(n, 10);
-        int req = t / gcd(q + (10 - q) / 10, t);
-        int nxt = ((r + req - 1) / req) * req;
-        int x = nxt - (nxt - 10) * (nxt / 10);
-        return q * 10 + x;
+        while(div_mul(n)%t != 0){
+            n++;
+        }
+        return n;
+        
     }
 };
