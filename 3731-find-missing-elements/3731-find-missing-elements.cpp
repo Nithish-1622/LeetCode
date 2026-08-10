eb6ad1c7f8c60490c0d523de964c756960de3fef
Nithish S <nithish.s.1622@gmail.com>
2026-08-10 09:10:52 +0530

    Time : 4 ms (39.14%)  Space : 32.1 MB (91.59%)  Date : 08-10-2026

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> findMissingElements(vector<int>& n) {
-        int mini = *min_element(n.begin(),n.end());
-        int maxi = *max_element(n.begin(),n.end());
-        vector <int> ans;
-        for(int i=mini;i<maxi;i++){
-            if(find(n.begin(),n.end(),i) == n.end()){
+    vector<int> findMissingElements(vector<int>& nums) {
+        int mini = *min_element(nums.begin(),nums.end());
+        int maxi = *max_element(nums.begin(),nums.end());
+        vector<int> ans;
+        for(int i=mini;i<=maxi;i++){
+            if(find(nums.begin(),nums.end(),i)==nums.end()){
                 ans.push_back(i);
             }
         }
         return ans;
-        
     }
 };
