20307a97640265390a87cf62a326c2b200fca553
Nithish S <nithish.s.1622@gmail.com>
2026-09-10 22:31:37 +0530

    Time : 346 ms (5.15%)  Space : 251.1 MB (5.02%)  Date : 09-10-2026

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -5,13 +5,10 @@ public:
     int sumSubarrayMins(vector<int>& arr) {
         stack<vector<int>> stack;
         vector<int> res(arr.size(), 0);
-
         for (int i = 0; i < arr.size(); i++) {
-            // use ">=" to deal with duplicate elements
             while (!stack.empty() && stack.top()[0] >= arr[i]) {
                 stack.pop();
             }
-
             int j = stack.empty() ? -1 : stack.top()[1];
             res[i] = stack.empty() ? arr[i] * (i + 1) : res[j] + arr[i] * (i - j);
             stack.push({arr[i], i});
