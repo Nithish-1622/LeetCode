07d6c1fd252c00cf8bf7d3865a86269999f6d795
Nithish S <nithish.s.1622@gmail.com>
2026-09-15 21:59:19 +0530

    Time : 42 ms (62.05%)  Space : 84.7 MB (83.5%)  Date : 09-15-2026

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> mp;
+        int l = 0;
+        int r = fruits.size();
+        int ans = 0;
+        for(int i=0;i<r;i++){
+            mp[fruits[i]]++;
+            while (mp.size() > 2){
+                mp[fruits[l]]--;
+                if (mp[fruits[l]] == 0){
+                    mp.erase(fruits[l]);
+                }
+                l++;
+
+            }
+            ans = max(ans, i-l+1);
+        }
+        return ans;
+
+    }
+};
