357c5d0dd10e7a6c5e1b142a2a25d68fc478e623
Nithish S <nithish.s.1622@gmail.com>
2026-09-23 21:05:25 +0530

    Time : 0 ms (100%)  Space : 8.3 MB (84.09%)  Date : 09-23-2026

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int l = 0;
+        int r = 0;
+        int n = word1.size();
+        int m = word2.size();
+        string mer;
+        while(l<n || r<m){
+            if(l<n){
+            mer += word1[l];
+            l++;
+            }
+            if(r<m){
+                mer += word2[r];
+                r++;
+            }
+
+        }
+        return mer;
+        
+    }
+};
